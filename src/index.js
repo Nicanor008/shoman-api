7aa2befaf856ecd3d651163f616e7c870244f7a9
Nicanor008 <nicanorkorir008@gmail.com>
2020-11-27 09:54:47 +0300

    api landing page

--- src/index.js
+++ src/index.js
@@ -1,53 +1,58 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const chalk = require("chalk");
-const mongoose = require("mongoose");
-const users = require("./routes/api/users");
-import emailVerify from "./routes/endpoints/users";
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const chalk = require('chalk')
+const mongoose = require('mongoose')
+const users = require('./routes/api/users')
+import emailVerify from './routes/endpoints/users'
 
-const app = express();
-const DB = require("./config/keys").mongoUri;
+const app = express()
+const DB = require('./config/keys').mongoUri
 
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+    bodyParser.urlencoded({
+        extended: false,
+    })
+)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(morgan("dev"));
+app.use(morgan('dev'))
 
 mongoose
-  .connect(DB, {
-    keepAlive: 1,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: false
-  })
-  .then(() => console.log(chalk.greenBright("connection to the database successful")))
-  .catch(err => console.log(err));
+    .connect(DB, {
+        keepAlive: 1,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: false,
+    })
+    .then(() =>
+        console.log(chalk.greenBright('connection to the database successful'))
+    )
+    .catch((err) => console.log(err))
 
-  const connectWithRetry = () => {
+const connectWithRetry = () => {
     console.log('MongoDB connection with retry')
     return mongoose.connect(DB, {
         keepAlive: 1,
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        autoIndex: false
-      })
-  }
+        autoIndex: false,
+    })
+}
 
-mongoose.connection.on("error", err => {
-  console.log(`MongoDB connection error: ${err}`);
-  setTimeout(connectWithRetry, 5000);
-});
+mongoose.connection.on('error', (err) => {
+    console.log(`MongoDB connection error: ${err}`)
+    setTimeout(connectWithRetry, 5000)
+})
 
-app.use("/api/v1/users", users);
-app.use("/verify-email", emailVerify);
+app.use('/api/v1/users', users)
+app.use('/verify-email', emailVerify)
+app.use('/', (req, res) => {
+    return res.send('<p>Welcome to <b>Shoman Mentorship Platform.</b></p>')
+})
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 app.listen(port, () =>
-  console.log(chalk.magenta(`server running on port ${port}`))
-);
+    console.log(chalk.magenta(`server running on port ${port}`))
+)
